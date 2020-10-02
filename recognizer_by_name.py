9b906253657d3ac6335f660650e370430d1ec539
Aarohi Singla <aarohisingla1987@gmail.com>
2020-10-02 19:46:23 +0530

    Add files via upload

---
+++ recognizer_by_name.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+#pip install opencv-contrib-python
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('model/trained_model2.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+font = cv2.FONT_HERSHEY_SIMPLEX
+id = 0
+names = ['None','asd','aaa'] 
+
+# Define min window size to be recognized as a face
+
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, img =cam.read()
+    #img = cv2.flip(img, -1) # Flip vertically
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5
+       )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        
+        # If confidence is less them 100 ==> "0" : perfect match 
+        if (confidence < 100):
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(
+                    img, 
+                    str(id), 
+                    (x+5,y-5), 
+                    font, 
+                    1, 
+                    (255,255,255), 
+                    2
+                   )
+        cv2.putText(
+                    img, 
+                    str(confidence), 
+                    (x+5,y+h-5), 
+                    font, 
+                    1, 
+                    (255,255,0), 
+                    1
+                   )  
+    
+    cv2.imshow('camera',img) 
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
